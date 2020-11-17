b5515dc42108ef3c6d745503868ef99c214e7f2f
wangkekai <kekai_wang@qingsongchou.com>
2020-11-17 17:52:57 +0800

    add 404 page， add middleware

--- go.mod
+++ go.mod
@@ -12,4 +12,9 @@ require (
 	github.com/facebookgo/stats v0.0.0-20151006221625-1b76add642e4 // indirect
 	github.com/facebookgo/subset v0.0.0-20200203212716-c811ad88dec4 // indirect
 	github.com/gin-gonic/gin v1.6.3
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- router/router.go
+++ router/router.go
@@ -4,9 +4,24 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kekaiwang/go_blog/router/middleware"
 )
 
 func SetupRouter(g *gin.Engine) {
+	g.Use(gin.Recovery())
+	g.Use(middleware.NoCache)
+	g.Use(middleware.Options)
+	g.Use(middleware.OrderAccess)
+
+	g.LoadHTMLGlob("web/*")
+
+	// 404 redirect
+	g.NoRoute(func(c *gin.Context) {
+		c.Header("Content-type", "text/html; charset=utf-8")
+
+		c.HTML(http.StatusNotFound, "index.html", nil)
+	})
+
 	g.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
