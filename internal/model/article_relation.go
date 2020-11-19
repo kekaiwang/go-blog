cebd0e6623a07b6fedbfc6d8890c10363f8c4934
kekaiwang <wkekai@163.com>
2020-11-19 22:08:10 +0800

    add article_relation create

--- internal/model/article_relation.go
+++ internal/model/article_relation.go
@@ -1,5 +1,7 @@
 package model
 
+import "github.com/kekaiwang/go_blog/pkg/drives"
+
 type ArticleRelation struct {
 	Id        int64 `gorm:"primary_key"`
 	ArticleId int64 `gorm:"article_id"` // 文章ID
@@ -14,4 +16,9 @@ func ArticleRelationModel() *ArticleRelation {
 
 func (ar *ArticleRelation) TableName() string {
 	return `article_relation`
+}
+
+//Create insert articleRelation
+func (ar *ArticleRelation) Create(articleRelation *ArticleRelation) error {
+	return drives.BlogDB.Create(articleRelation).Error
 }
