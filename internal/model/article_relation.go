1d6434b3929ea8df947067927073087af45c79ab
wangkekai <kekai_wang@qingsongchou.com>
2020-11-19 19:20:22 +0800

    complete article_relation

--- internal/model/article_relation.go
+++ internal/model/article_relation.go
@@ -1,18 +1,18 @@
 package model
 
 type ArticleRelation struct {
-	Id         int64  `gorm:"primary_key"`
-	Name       string `gorm:"name" json:"name"` // 分类名字
-	RouterLink string `gorm:"router_link"`
+	Id        int64 `gorm:"primary_key"`
+	ArticleId int64 `gorm:"article_id"` // 文章ID
+	TagId     int64 `gorm:"tag_id"`     // 标签ID
+
 }
 
-// CREATE TABLE `wkk_category` (
-// 	`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-// 	`name` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '分类名字',
-// 	`router_link` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '路由地址',
-// 	`link_article` int(11) unsigned DEFAULT '0' COMMENT '关联文章数量',
-// 	`status` tinyint(1) DEFAULT NULL COMMENT '1:正常 2:禁用 3:已删除',
-// 	`created` datetime DEFAULT NULL COMMENT '创建时间',
-// 	`updated` datetime DEFAULT NULL COMMENT '更新时间',
-// 	PRIMARY KEY (`id`)
-//   ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COMMENT='文章分类表';
+var articleRelation *ArticleRelation
+
+func ArticleRelationModel() *ArticleRelation {
+	return articleRelation
+}
+
+func (ar *ArticleRelation) TableName() string {
+	return `article_relation`
+}
