e91c87b6c69441e234979d47b87e3539fb9c936f
kekaiwang <wkekai@163.com>
2023-02-21 21:45:59 +0800

    add: user admin login

--- internal/service/user/user.go
+++ internal/service/user/user.go
@@ -39,3 +39,33 @@ func (u LoginRequest) Login(ctx *gin.Context) (*LoginResponse, *errs.ErrNo) {
 
 	return res, nil
 }
+
+func (u LoginRequest) AdminLogin(ctx *gin.Context) (*LoginResponse, *errs.ErrNo) {
+	fmt.Println(ctx.ClientIP())
+
+	// 1. md5 pass
+	pass := tools.MD5(u.UserName, u.Password, "$&)w")
+
+	// 2. get user info
+	var user model.AdminUser
+	info, err := user.GetUser(" name = ? AND password = ? ", []interface{}{u.UserName, pass})
+	if err != nil {
+		return nil, errs.ErrQueryModel
+	}
+
+	res := &LoginResponse{
+		ID:       int64(info.ID),
+		Password: info.Password,
+	}
+
+	// 3. update login info
+	info.LastIp = ctx.ClientIP()
+	info.LastLogin = time.Now()
+	info.LoginCount += 1
+	_, err = info.UpdateLogin()
+	if err != nil {
+		return nil, errs.ErrQueryModel
+	}
+
+	return res, nil
+}
