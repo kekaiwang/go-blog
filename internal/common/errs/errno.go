c709456032f13500f672ff032c6a1696260ce099
kekaiwang <wkekai@163.com>
2024-02-26 10:31:44 +0800

    modify: err msg

--- internal/common/errs/errno.go
+++ internal/common/errs/errno.go
@@ -4,6 +4,7 @@ var (
 	OK    = &ErrNo{Code: 0, Message: "OK"}
 	Error = &ErrNo{Code: 1, Message: "ERROR"}
 
+	// enum err msg
 	ErrInvalidParam        = &ErrNo{Code: 1001, Message: "Invalid params"}
 	ErrBindJson            = &ErrNo{Code: 1002, Message: "Bind json error"}
 	ErrRecordNotFound      = &ErrNo{Code: 1003, Message: "Record not found"}
