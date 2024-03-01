ca47c41c2ca49e2c9d02a49115535ee89fb4a18e
kekaiwang <wkekai@163.com>
2024-03-01 09:40:49 +0800

    modify: get page list;

--- internal/service/page/page.go
+++ internal/service/page/page.go
@@ -26,3 +26,24 @@ func (req *PageReq) GetPageInfo() (*PageRes, error) {
 
 	return res, nil
 }
+
+func (req *PageReq) GetPageList() (*PageRes, error) {
+	var (
+		res *PageRes
+	)
+	// get page info
+	var p model.PageInfo
+	data, err := p.GetPageBySlug(req.Slug)
+	if err != nil {
+		return nil, err
+	}
+
+	// res
+	res = &PageRes{
+		Name:    data.Name,
+		Slug:    data.Slug,
+		Content: template.HTML(data.Content),
+	}
+
+	return res, nil
+}
