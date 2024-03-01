04fd159d770c8946deee7808b49f388c6bfd6190
kekaiwang <wkekai@163.com>
2024-03-01 09:43:55 +0800

    modify: get category info;

--- internal/service/article/article.go
+++ internal/service/article/article.go
@@ -8,7 +8,7 @@ import (
 	"github.com/kekaiwang/go-blog/internal/model"
 )
 
-//GetArticleList
+// GetArticleList
 func (req *GetIndexArticleReq) GetArticleList() (*IndexArticleRes, error) {
 	var (
 		res        IndexArticleRes
@@ -132,6 +132,23 @@ func (req *ArticleDetailReq) ArticleDetail() (*ArticleDetailRes, error) {
 // getCategory.
 func getCategory() map[int64]*model.Category {
 	var c model.Category
+	categories, err := c.GetAll()
+	if err != nil {
+		return nil
+	}
+
+	cMap := make(map[int64]*model.Category)
+	for _, c := range categories {
+		cMap[c.ID] = c
+	}
+
+	return cMap
+}
+
+// getCategoryInfo. get category info
+func getCategoryInfo() map[int64]*model.Category {
+	var c model.Category
+
 	categories, err := c.GetAll()
 	if err != nil {
 		return nil
