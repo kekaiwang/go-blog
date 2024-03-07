f59d1c9eed0c2bf8ee3fcc8f23159b629c9a3eb4
kekaiwang <wkekai@163.com>
2024-03-07 09:50:17 +0800

    modify: get ta list

--- app/api/category.go
+++ app/api/category.go
@@ -193,3 +193,66 @@ func GetNewTagList(ctx *gin.Context) {
 		"meta":          meta,
 	})
 }
+
+// GetNewsTagList. get tag list
+func GetNewsTagList(ctx *gin.Context) {
+	ctx.Header("Content-type", "text/html; charset=utf-8")
+
+	// var params
+	var (
+		link    = ctx.Param("link")
+		req     category.GetTagReq
+		page    int64
+		err     error
+		limit   = int64(10)
+		pageStr = ctx.Query("page")
+	)
+
+	if link == "" {
+		ctx.HTML(http.StatusOK, "error.html", gin.H{
+			"Title": "Kekai Wang",
+		})
+		return
+	}
+
+	// parse page
+	if pageStr != "" {
+		page, err = strconv.ParseInt(pageStr, 10, 64)
+		if err != nil {
+			ctx.HTML(http.StatusOK, "error.html", nil)
+			return
+		}
+	}
+
+	if page == 0 {
+		page = 1
+	}
+	req.Offset = (page - 1) * limit
+	req.Limit = limit
+	req.Link = link
+
+	// get tag list
+	data, err := req.GetTagList()
+	if err != nil {
+		ctx.HTML(http.StatusOK, "error.html", gin.H{
+			"Title": "Kekai Wang",
+		})
+		return
+	}
+
+	meta := category.Meta{
+		Name: "标签|tag",
+		Type: ctx.Query("type"),
+	}
+
+	ctx.HTML(http.StatusOK, "category.html", gin.H{
+		"data":          data.Data,
+		"Title":         data.Name,
+		"total":         data.Total,
+		"total_page":    tools.NewTotalPage(data.Total, limit),
+		"current_page":  page,
+		"next_page":     page + 1,
+		"previous_page": page - 1,
+		"meta":          meta,
+	})
+}
