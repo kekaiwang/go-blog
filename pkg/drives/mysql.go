2218be1eeb47f764848324cd5200a4f054b8d087
kekaiwang <wkekai@163.com>
2024-03-10 17:56:47 +0800

    modify: img db

--- pkg/drives/mysql.go
+++ pkg/drives/mysql.go
@@ -15,11 +15,44 @@ var (
 
 func BlogDBInit() {
 	initBlogDB()
+	initBlogImgDB()
 }
 
 // layze init
 // init db
 func initBlogDB() {
+	var (
+		conf       = config.Get()
+		blogDBOnce sync.Once
+		initDB     func()
+	)
+
+	// init DB
+	initDB = func() {
+		db, err := gorm.Open("mysql", conf.Mysql.Uri)
+		if err != nil {
+			panic(err)
+		}
+
+		BlogDB = db
+		BlogDB.DB().SetMaxOpenConns(conf.Mysql.MaxOpenConns)
+		BlogDB.DB().SetMaxIdleConns(conf.Mysql.MaxIdleConns)
+		BlogDB.DB().SetConnMaxLifetime(time.Minute * time.Duration(conf.Mysql.ConnMaxLifeTime))
+		BlogDB.LogMode(conf.Mysql.LogMode)
+	}
+
+	if BlogDB == nil {
+		blogDBOnce.Do(initDB)
+	} else if err := BlogDB.DB().Ping(); err != nil {
+		initDB()
+	} else if err := BlogDB.Error; err != nil {
+		panic(err)
+	}
+}
+
+// layze init
+// initBlogImgDB db
+func initBlogImgDB() {
 	var (
 		conf       = config.Get()
 		blogDBOnce sync.Once
