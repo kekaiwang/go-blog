9b316250997269aa3c672e832cb925ae87b8574d
kekaiwang <wkekai@163.com>
2024-03-21 09:49:14 +0800

    modify: add inter res;

--- app/api/response.go
+++ app/api/response.go
@@ -7,3 +7,11 @@ type ApiResponse struct {
 	Code int         `json:"code"` // code
 	Next interface{} `json:"next"` // next info
 }
+
+// InterfaceResponse formate interface info
+type InterfaceResponse struct {
+	Data interface{} `json:"data"` // data info
+	Msg  string      `json:"msg"`  // success or err msg
+	Code int         `json:"code"` // code
+	Next interface{} `json:"next"` // next info
+}
