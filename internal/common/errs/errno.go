ad859027ddcc902281a42cb8dab737882e97ba3c
kekaiwang <wkekai@163.com>
2024-03-22 09:50:21 +0800

    modify: record not found

--- internal/common/errs/errno.go
+++ internal/common/errs/errno.go
@@ -5,9 +5,9 @@ var (
 	Error = &ErrNo{Code: 1, Message: "ERROR"}
 
 	// enum err msg
-	ErrInvalidParam        = &ErrNo{Code: 1001, Message: "Invalid params"}  // invalid params
-	ErrBindJson            = &ErrNo{Code: 1002, Message: "Bind json error"} // err bind json
-	ErrRecordNotFound      = &ErrNo{Code: 1003, Message: "Record not found"}
+	ErrInvalidParam        = &ErrNo{Code: 1001, Message: "Invalid params"}   // invalid params
+	ErrBindJson            = &ErrNo{Code: 1002, Message: "Bind json error"}  // err bind json
+	ErrRecordNotFound      = &ErrNo{Code: 1003, Message: "Record not found"} // record not found
 	ErrQueryModel          = &ErrNo{Code: 1004, Message: "Query model err."}
 	ErrUpdateRecord        = &ErrNo{Code: 1005, Message: "update model err."}
 	ErrRecordAlreadyExists = &ErrNo{Code: 1006, Message: "Already exists."}
