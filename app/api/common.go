94c07c12adbbfc2b87cb13e9e19b15630eb16de8
kekaiwang <wkekai@163.com>
2024-04-16 09:39:42 +0800

    modify: ping api

--- app/api/common.go
+++ app/api/common.go
@@ -9,7 +9,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// HealthPing. health ping
+// HealthPing. health ping api
 func HealthPing(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, "PONG") // pong
 }
