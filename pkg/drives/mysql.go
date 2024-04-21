3a84dfbae03fc0f778c32e0708ffb4cf66141fac
kekaiwang <wkekai@163.com>
2024-04-22 06:45:31 +0800

    modify: log model

--- pkg/drives/mysql.go
+++ pkg/drives/mysql.go
@@ -71,7 +71,7 @@ func initBlogImgDB() {
 		BlogDB.DB().SetMaxOpenConns(conf.Mysql.MaxOpenConns)                                    // max connect
 		BlogDB.DB().SetMaxIdleConns(conf.Mysql.MaxIdleConns)                                    // max idel connect
 		BlogDB.DB().SetConnMaxLifetime(time.Minute * time.Duration(conf.Mysql.ConnMaxLifeTime)) // max life
-		BlogDB.LogMode(conf.Mysql.LogMode)
+		BlogDB.LogMode(conf.Mysql.LogMode)                                                      // log
 	}
 
 	if BlogDB == nil {
