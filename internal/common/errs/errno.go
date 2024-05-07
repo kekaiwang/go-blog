ecff7027e64e7c3aced7e4412dcb15c80fc34cd4
kekaiwang <wkekai@163.com>
2024-05-07 09:24:24 +0800

    modify: err update

--- internal/common/errs/errno.go
+++ internal/common/errs/errno.go
@@ -5,11 +5,11 @@ var (
 	Error = &ErrNo{Code: 1, Message: "ERROR"}
 
 	// enum err msg
-	ErrInvalidParam        = &ErrNo{Code: 1001, Message: "Invalid params"}   // invalid params
-	ErrBindJson            = &ErrNo{Code: 1002, Message: "Bind json error"}  // err bind json
-	ErrRecordNotFound      = &ErrNo{Code: 1003, Message: "Record not found"} // record not found
-	ErrQueryModel          = &ErrNo{Code: 1004, Message: "Query model err."} // query err
-	ErrUpdateRecord        = &ErrNo{Code: 1005, Message: "update model err."}
+	ErrInvalidParam        = &ErrNo{Code: 1001, Message: "Invalid params"}    // invalid params
+	ErrBindJson            = &ErrNo{Code: 1002, Message: "Bind json error"}   // err bind json
+	ErrRecordNotFound      = &ErrNo{Code: 1003, Message: "Record not found"}  // record not found
+	ErrQueryModel          = &ErrNo{Code: 1004, Message: "Query model err."}  // query err
+	ErrUpdateRecord        = &ErrNo{Code: 1005, Message: "update model err."} // err update
 	ErrRecordAlreadyExists = &ErrNo{Code: 1006, Message: "Already exists."}
 	ErrRecordyExists       = &ErrNo{Code: 1006, Message: "Already exists."}
 	ErrRawsFound           = &ErrNo{Code: 1008, Message: "Raws count err."} // raws count err
