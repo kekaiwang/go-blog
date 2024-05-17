a61f7755d65c08269fa6a313c38f0efabb7190ca
kekaiwang <wkekai@163.com>
2024-05-17 08:07:18 +0800

    modify: model;

--- internal/model/article_relation.go
+++ internal/model/article_relation.go
@@ -45,6 +45,7 @@ func (ar *ArticleRelation) GetARByTagId(id int64) ([]int, error) {
 
 // GetARByTagID
 func (ar *ArticleRelation) GetARByTagID(id int64) ([]int, error) {
+	// article id pluck
 	articleIds := []int{}
 
 	err := drives.BlogDB.Table(ar.TableName()).Where("tag_id = ? ", id).Pluck("article_id", &articleIds).Error
