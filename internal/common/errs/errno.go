cd59b68d2154ade5dca8d9b3f60f9828c2329d9e
kekaiwang <wkekai@163.com>
2024-05-30 08:00:42 +0800

    modify: 404 page

--- internal/common/errs/errno.go
+++ internal/common/errs/errno.go
@@ -10,7 +10,7 @@ var (
 	ErrRecordNotFound      = &ErrNo{Code: 1003, Message: "Record not found"}  // record not found
 	ErrQueryModel          = &ErrNo{Code: 1004, Message: "Query model err."}  // query err
 	ErrUpdateRecord        = &ErrNo{Code: 1005, Message: "update model err."} // err update
-	ErrRecordAlreadyExists = &ErrNo{Code: 1006, Message: "Already exists."}
+	ErrRecordAlreadyExists = &ErrNo{Code: 1006, Message: "Already exists."}   // already exists
 	ErrRecordyExists       = &ErrNo{Code: 1006, Message: "Already exists."}
 	ErrRawsFound           = &ErrNo{Code: 1008, Message: "Raws count err."} // raws count err
 )
