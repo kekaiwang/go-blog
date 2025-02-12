efd1ddfac76bbc4ec5db9bc48cba733dfb355174
kekaiwang <wkekai@163.com>
2025-02-12 16:53:33 +0800

    modify: base model define

--- internal/model/base.go
+++ internal/model/base.go
@@ -1,1 +1,15 @@
 package model
+
+import "gorm.io/gorm"
+
+type (
+	FindCall func(db *gorm.DB)
+)
+
+type RowsCount struct {
+	Total int64 `gorm:"column:total"`
+}
+
+const (
+	MaxBatchSize = 500
+)
