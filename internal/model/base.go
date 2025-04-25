638da9740111a3fe3f51e9032640b407684ff4da
kekaiwang <wkekai@163.com>
2025-04-25 17:21:13 +0800

    modfiy: push base

--- internal/model/base.go
+++ internal/model/base.go
@@ -121,3 +121,10 @@ func CreateOnes[T any](tx *gorm.DB, obj *T) (err error) {
 	}
 	return
 }
+
+func CreateBatch[T any](tx *gorm.DB, obj *T) (err error) {
+	if err := tx.Model(new(T)).Create(obj); err.Error != nil {
+		return err.Error
+	}
+	return
+}
