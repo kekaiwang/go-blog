a3945cdb3ad6bbb593544a506bdd5d0f875c8321
kekaiwang <wkekai@163.com>
2025-05-26 13:31:24 +0800

    modify: tag detail

--- internal/model/tag.go
+++ internal/model/tag.go
@@ -106,3 +106,13 @@ func (t *Tag) UpdateTagInfo() (int64, error) {
 
 	return int64(result.RowsAffected), nil
 }
+
+// UpdateTag.
+func (t *Tag) UpdateTagDetail() (int64, error) {
+	result := drives.BlogDB.Model(&t).Update(t)
+	if result.Error != nil {
+		return 0, result.Error
+	}
+
+	return int64(result.RowsAffected), nil
+}
