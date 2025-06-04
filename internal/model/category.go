5e2ba2237b0b58ad9a2d1bbed3a0fa1f81d12ebe
kekaiwang <wkekai@163.com>
2025-06-04 14:33:57 +0800

    modify: category info

--- internal/model/category.go
+++ internal/model/category.go
@@ -112,3 +112,13 @@ func (c *Category) UpdateCategory() (int64, error) {
 
 	return result.RowsAffected, nil
 }
+
+// UpdateCategory. update category
+func (c *Category) UpdateCategoryBy() (int64, error) {
+	result := drives.BlogDB.Model(&c).Update(c)
+	if result.Error != nil {
+		return 0, result.Error
+	}
+
+	return result.RowsAffected, nil
+}
