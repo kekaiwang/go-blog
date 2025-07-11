7c7b8571287504e3575e824367bd0dec0de8d49f
kekaiwang <wkekai@163.com>
2025-07-11 10:38:23 +0800

    modify: article info

--- internal/model/article.go
+++ internal/model/article.go
@@ -155,3 +155,15 @@ func (a *Article) GetArticlesCTINFOs(query string, args []interface{}, limit, of
 
 	return articles, nil
 }
+
+// GetArticlesByCTId
+func (a *Article) GetArticlesCTInfo(query string, args []interface{}, limit, offset int64) ([]*Article, error) {
+	articles := []*Article{}
+
+	err := drives.BlogDB.Table(a.TableName()).Where(query, args...).Limit(limit).Offset(offset).Order("display_time DESC").Find(&articles).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return articles, nil
+}
