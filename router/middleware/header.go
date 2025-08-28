f4ecc56f38ff8074c30c332bcfea0811f01ee9a0
kekaiwang <wkekai@163.com>
2025-08-28 10:24:29 +0800

    modify: moid

--- router/middleware/header.go
+++ router/middleware/header.go
@@ -48,6 +48,18 @@ func Secure(c *gin.Context) {
 	}
 }
 
+// Secure is a middleware function that appends security
+// and resource access headers.
+func Secures(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("X-Frame-Options", "DENY")
+	c.Header("X-Content-Type-Options", "nosniff")
+	c.Header("X-XSS-Protection", "1; mode=block")
+	if c.Request.TLS != nil {
+		c.Header("Strict-Transport-Security", "max-age=31536000")
+	}
+}
+
 // across about
 func OrderAccess(c *gin.Context) {
 	c.Header("Access-Control-Allow-Credentials", "true")
