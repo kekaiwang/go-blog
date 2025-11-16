f65b8388e4fc3db0b124743f91ef18f555e4f6f7
kekaiwang <wkekai@163.com>
2025-11-16 15:28:37 +0800

    modify: add red

--- utils/format/print.go
+++ utils/format/print.go
@@ -38,3 +38,13 @@ func printColor(str string, color int32) {
 func printColors(str string, color int32) {
 	fmt.Printf("%c[0;0;%vm%s%c[0m\n", 0x1B, color, str, 0x1B)
 }
+
+
+/**
+ * print color string
+ * @params str
+ * @params color
+ */
+func printColorRed(str string, color int32) {
+	fmt.Printf("%c[0;0;%vm%s%c[0m\n", 0x1B, color, str, 0x1B)
+}
